8c993cbb1577140c9d117ffca76e8461c83a0e5d
Billy Todd <billy.todd50@yahoo.com>
2019-06-23 01:52:46 +0000

    add more libraries, landing route set-up, add navbar

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const express = require('express'),
-	  app = express();
+const express 		= require('express'),
+	  app 			= express(),
+	  mongoose 		= require('mongoose'),
+	  bodyParser 	= require('body-parser');
+
+app.get('/', (req, res) => {
+	res.render('landing.ejs');
+});
+
+app.listen(3000, () => {
+	console.log('AHouse is Running!');
+});
