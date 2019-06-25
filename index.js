4efc87cf48a92f12d14eff98f80794721c4e11d6
Billy Todd <billy.todd50@yahoo.com>
2019-06-25 15:56:46 +0000

    bulma to bootstrap, add about and pricing page, redo landing

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express 		= require('express'),
 	  app 			= express(),
-	  mongoose 		= require('mongoose'),
 	  bodyParser 	= require('body-parser');
 
+
+
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-	res.render('landing.ejs');
+	res.render('landing');
 });
 
-app.listen(3000, () => {
-	console.log('AHouse is Running!');
+app.get('/about', (req, res) => {
+	res.render('about');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
