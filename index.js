b2980093f6e238227861ba2844bc2e51b3fd4ba3
Billy Todd <billy.todd50@yahoo.com>
2019-07-26 19:00:10 +0000

    update

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.post('/', (req, res)  => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
